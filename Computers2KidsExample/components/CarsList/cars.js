3e689be4375eefc4326062289502161f1780f416
Joe Miller <josephthollmiller@gmail.com>
2021-06-14 11:35:30 -0700

    Add files via upload

---
+++ Computers2KidsExample/components/CarsList/cars.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    name: "Apple",
+    tagline: "Starting at $600",
+    image: require("../../assets/images/ModelS.jpeg"),
+  },
+  {
+    name: "Dell",
+    tagline: "Starting at $500",
+
+    image: require("../../assets/images/Model3.jpeg"),
+  },
+  {
+    name: "Lenovo",
+    tagline: "Starting at $400",
+
+    image: require("../../assets/images/ModelX.jpeg"),
+  },
+  {
+    name: "Acer",
+    tagline: "Starting at $300",
+
+    image: require("../../assets/images/ModelY.jpeg"),
+  },
+];
